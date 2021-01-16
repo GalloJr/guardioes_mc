2ddcd1cf70d3f749367123ec98562818c71def5f
GalloJr <renato.gallo@fatec.sp.gov.br>
2021-01-16 14:01:40 -0300

    Project created

---
+++ backend/src/main/java/com/gallo/guardioes_mc/GuardioesMcApplication.java
@@ -0,0 +1,13 @@
+package com.gallo.guardioes_mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardioesMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardioesMcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gallo/guardioes_mc/GuardioesMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallo.guardioes_mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardioesMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
