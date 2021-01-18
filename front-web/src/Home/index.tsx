5d6ec88fe8fba08c9f77df05de7d1692c9374357
GalloJr <renato.gallo@fatec.sp.gov.br>
2021-01-18 19:45:28 -0300

    Fix images Home

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,6 @@
 import './styles.css';
-import { ReactComponent as MainImage } from './main.svg';
-import { ReactComponent as MembersImage } from './members.svg';
+import logoImage from  './logo.png';
+import membersImage from  './members.png';
 import { Link } from 'react-router-dom';
 
 function Home() {
@@ -22,17 +22,17 @@ function Home() {
                          e 3 membros Old Dogs que já são membros a mais de 15 anos.
                     </h3>
                         <div className="home-image">
-                            <MembersImage />
+                            <img src={membersImage} alt="logo" />
                         </div>
                         <h5 className="home-address">
                             Sede <br /> R. Manoel Bôsco Ribeiro, 46 - Jardim das Industrias, São José dos Campos - SP, 12241-070
                     </h5>
                     </div>
                     <div className="home-image">
-                        <MainImage />
+                        <img src={logoImage} alt="logo" />
                         <Link to="/members" className="home-btn-member">
                             INTEGRANTES
-                    </Link>
+                        </Link>
                     </div>
                 </div>
             </div>
