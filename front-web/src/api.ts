fdca028f1c3a042891f42cf90b24d19adda2c8d5
GalloJr <renato.gallo@fatec.sp.gov.br>
2021-01-18 19:11:33 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const API_URL = 'https://guardioes-mc.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export function fetchMembers() {
     return axios(`${API_URL}/members`);
 }
+
+
